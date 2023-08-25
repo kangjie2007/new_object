37c223c52db9e160c0f98818b676a3782a2d9a29
kangjie k <368662975@qq.com>
2023-08-25 09:06:11 +0000

    .

--- project/app1/views.py
+++ project/app1/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib import admin
-
+from app1.models import Account_cs
 # Create your views here.
 def main(requests):
-    return HttpResponse("app-1-main")
+    _all=Account_cs.objects.all()[0]
+    print(_all.username)
+    return HttpResponse(str(_all.username))
